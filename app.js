694fc4f026e0961e4dbcf6ac9c1149316142df6c
Veysel Aydin <weycell.aydin09@gmail.com>
2023-02-05 14:08:41 +0300

    darling

---
+++ app.js
@@ -0,0 +1,5 @@
+const etiketBtn = document.querySelector("#etiket");
+const homeBtn = document.querySelector("#home");
+const aboutBtn = document.querySelector("#about");
+const portfolioBtn = document.querySelector("#portfolio");
+const contactBtn = document.querySelector("#contact");
